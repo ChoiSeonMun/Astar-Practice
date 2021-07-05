c8163e2ff7166d3bd6a4adb8faea4968675ac5e0
SeonMun Choi <seonmun.choi@gmail.com>
2021-07-05 17:20:38 +0900

    Construct the main loop of program

---
+++ AstarExample/AstarExample/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace AstarExample
+{
+    class Program
+    {
+        static readonly int WAIT_TICK = 1000 / 30;
+
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+
+            int lastTick = 0;
+            while (true)
+            {
+                #region Frame Management
+                int currentTick = Environment.TickCount;
+                int deltaTick = currentTick - lastTick;
+                if (deltaTick < WAIT_TICK)
+                {
+                    continue;
+                }
+
+                lastTick = currentTick;
+                #endregion
+
+                #region Update
+                
+                #endregion
+
+                #region Render
+                Console.SetCursorPosition(0, 0);
+                #endregion
+            }
+        }
+    }
+}
